8340b2996c638d539ff1b7035c33d983a1179ffb
David Contreras <dvd.cnt@gmail.com>
2022-11-24 09:47:31 -0600

    uses sum on a 3D array directly
    Changed to a single assignment by using the function cat on the 3rd dimension

--- Conway.m
+++ Conway.m
@@ -8,32 +8,39 @@ N = 20; % number of rows
 M = 25; % number of columns
 
 rows = 1:N;
-topRows = mod(rows-2,N)+1;
-bottomRows = mod(rows,N)+1;
+topRows = mod(rows-2, N) + 1;
+bottomRows = mod(rows, N) + 1;
 
 cols = 1:M;
-leftCols = mod(cols-2,M)+1;
-rightCols = mod(cols,M)+1;
+leftCols = mod(cols-2, M) + 1;
+rightCols = mod(cols, M) + 1;
 
+# Random start
 grid = randi([0,1], N, M);
-k = zeros(N, M, 8); % neighbours
 
 imshow(grid)
 axis('equal')
 title('0')
 
 for n = 1:100
-  k(:,:,1) = grid(topRows, leftCols); % top left
-  k(:,:,2) = grid(topRows, cols); % top
-  k(:,:,3) = grid(topRows, rightCols); % top right
-  k(:,:,4) = grid(rows, leftCols); % left
-  k(:,:,5) = grid(rows, rightCols); % right
-  k(:,:,6) = grid(bottomRows, leftCols); % bottom left
-  k(:,:,7) = grid(bottomRows, cols); % bottom
-  k(:,:,8) = grid(bottomRows, rightCols); % bottom right
-  neighbours = sum(k,3);
+  # Get the 8 neighbours on a 3D array and sum them on the 3rd dimension
+  neighbours = sum(
+    cat(
+    3,
+    grid(topRows, leftCols),
+    grid(topRows, cols),
+    grid(topRows, rightCols),
+    grid(rows, leftCols),
+    grid(rows, rightCols),
+    grid(bottomRows, leftCols),
+    grid(bottomRows, cols),
+    grid(bottomRows, rightCols)
+    )
+  ,3);
+
   # Conways game of life
   grid = ((grid & neighbours==2) | neighbours==3 );
+
   # Display
   imshow(grid)
   title(n)
